e5c3e0708d71e96cd2982e20594bbdd431a8fb28
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-05-17 08:55:57 -0400

    New project: practice portfolio

---
+++ includes/router.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+function normalizeuri(string $uri): string{
+    $uri = strtolower(trim($uri, '/'));
+
+    return $uri === '' ? 'index' : $uri;
+};
+
+function dispatch(string $uri, string $method): void{
+
+}
