079b8e0cbfbc50646ff3be98a89503e2750c6753
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-05-19 22:59:13 -0400

    GET AND POST: These two methods will be handled when creating our request

--- includes/router.php
+++ includes/router.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
+const ALLOWED_METHODS =['GET', 'POST'];
+const INDEX_URI = '';
+
 function normalizeuri(string $uri): string{
     $uri = strtolower(trim($uri, '/'));
 
-    return $uri === '' ? 'index' : $uri;
+    return $uri === INDEX_URI ? 'index' : $uri;
 };
 
 function dispatch(string $uri, string $method): void{
