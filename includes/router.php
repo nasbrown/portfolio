72ec9b1234b9162c7df512e9ce97381e52b5d9b0
Naswade21 <120067170+Naswade21@users.noreply.github.com>
2026-05-22 13:06:01 -0400

    Completed: Going to take fundamentals of this project to build a task-list

--- includes/router.php
+++ includes/router.php
@@ -7,6 +7,7 @@ const INDEX_URI = '';
 const INDEX_ROUTE = 'index';
 
 function normalizeuri(string $uri): string{
+    $uri = strtok($uri, '?');
     $uri = strtolower(trim($uri, '/'));
 
     return $uri === INDEX_URI ? INDEX_ROUTE : $uri;
--- templates/guestbook_get.php
+++ templates/guestbook_get.php
@@ -1,5 +1,25 @@
 <?php
 
+function obName(string $name): string{
+    if(strlen($name) <= 2){
+        return $name;
+    }
+
+    return substr($name, 0, 1) . str_repeat('*', 
+    strlen($name) - 2) . substr($name, -1);
+    }
+
+function obEmail(string $email): string{
+    $parts = explode('@', $email);
+    $username = $parts[0];
+    $domain = $parts[1] ?? '';
+    $username = substr($username, 0, 2) . str_repeat('*', 
+    max(0, strlen($username) - 2));
+
+    return "$username@$domain";
+}
+
+
 $messages = $data['messages'];
 
 ?>
@@ -10,8 +30,8 @@ $messages = $data['messages'];
         <p>There are no messages here yet.. Please leave a message!</p>
     <?php else: ?>
         <?php foreach($messages as $message): ?>
-            <h3>User : <?= htmlspecialchars($message['name']) ?></h3>
-            <p>Email : <?= htmlspecialchars($message['email']) ?></p>
+            <h3>User : <?= obName(htmlspecialchars($message['name'])) ?></h3>
+            <p>Email : <?= obEmail(htmlspecialchars($message['email'])) ?></p>
             <p>Message : <?= nl2br(htmlspecialchars($message['message'])) ?></p>
             <small>Posted on: <?= htmlspecialchars($message['created_at']) ?></small>
         <?php endforeach; ?>
